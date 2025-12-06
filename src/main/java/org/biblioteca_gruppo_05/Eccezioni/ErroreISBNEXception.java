8dd91d2dcee4c4f4cc653fbc277fef440499710d
danie <danielvita888@gmail.com>
2025-12-06 15:51:25 +0100

    Eccezioni

---
+++ src/main/java/org/biblioteca_gruppo_05/Eccezioni/ErroreISBNEXception.java
@@ -0,0 +1,7 @@
+package org.biblioteca_gruppo_05.Eccezioni;
+
+public class ErroreISBNEXception extends RuntimeException {
+    public ErroreISBNEXception(String message) {
+        super(message);
+    }
+}
