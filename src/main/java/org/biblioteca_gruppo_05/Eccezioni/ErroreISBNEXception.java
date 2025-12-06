3bcba5effd659a5b32e8bf98e23c083b10e8cfd4
danie <danielvita888@gmail.com>
2025-12-06 17:04:05 +0100

    Archivio Profili

--- src/main/java/org/biblioteca_gruppo_05/Eccezioni/ErroreISBNEXception.java
+++
@@ -1,7 +0,0 @@
-package org.biblioteca_gruppo_05.Eccezioni;
-
-public class ErroreISBNEXception extends RuntimeException {
-    public ErroreISBNEXception(String message) {
-        super(message);
-    }
-}
