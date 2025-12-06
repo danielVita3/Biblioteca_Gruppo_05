91b7e638fa4d975d9f6378c8f3d54b3d1166d184
Lenovo <c.sabao3@studenti.unisa.it>
2025-12-06 17:24:20 +0100

    Aggiornamento classe ArchivioLibri

--- src/main/java/org/biblioteca_gruppo_05/Gestione_Libri/ArchivioLibri.java
+++ src/main/java/org/biblioteca_gruppo_05/Gestione_Libri/ArchivioLibri.java
@@ -2,8 +2,7 @@ package org.biblioteca_gruppo_05.Gestione_Libri;
 
 import org.biblioteca_gruppo_05.Eccezioni.*;
 
-import java.io.IOException;
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -32,6 +31,11 @@ public class ArchivioLibri implements Serializable {
     public ArchivioLibri(String fileName){
         this.libri = new LinkedHashMap<>();
         this.fileName = fileName;
+        try{
+            leggiDaFile();
+        }catch(ErroreLetturaFileException e){
+            System.err.println("Archivio vuoto" + e.getMessage());
+        }
     }
 
     /**
@@ -43,18 +47,38 @@ public class ArchivioLibri implements Serializable {
      * @pre l non deve essere null .
      * @post Il libro viene aggiunto alla mappa.
      */
-    public void aggiungiLibro(Libro l) throws LibroEsistenteException {};
+    public void aggiungiLibro(Libro l) throws LibroEsistenteException {
+        if(libri.containsKey(l.getISBN())){
+            throw new LibroEsistenteException("Libro con ISBN" + l.getISBN() + "già presente.");
+        }
+        libri.put(l.getISBN(),l);
+        try{
+            salvaSuFile();
+        }catch(ErroreScritturaFileException e){
+            System.err.println("Errore di scrittura" +  e.getMessage());
+        }
+    }
 
     /**
      * @brief Rimuove un libro esistente dall'archivio.
      *
-     * @param l L'oggetto Libro da rimuovere.
+     * @param ISBN L'ISBN del libro da rimuovere.
      * @throws LibroNonTrovatoException Se il libro non è presente o non può essere rimosso.
      *
-     * @pre l non deve essere null.
+     * @pre ISBN non deve essere null.
      * @post Il libro viene rimosso dalla mappa.
      */
-    public void rimuoviLibro(Libro l) throws LibroNonTrovatoException {};
+    public void rimuoviLibro(String ISBN) throws LibroNonTrovatoException {
+        if(!libri.containsKey(ISBN)){
+            throw new LibroNonTrovatoException("Impossibile rimuovere: Libro con ISBN " + l.getISBN() + "non trovato");
+        }
+        libri.remove(ISBN);
+        try{
+            salvaSuFile();
+        }catch(ErroreScritturaFileException e){
+            System.err.println("Errore di scrittura" +  e.getMessage());
+        }
+    }
 
     /**
      * @brief Cerca un singolo libro tramite il codice ISBN.
@@ -66,9 +90,14 @@ public class ArchivioLibri implements Serializable {
      * @pre ISBN deve essere non null.
      * @post Restituisce il libro senza rimuoverlo o modificarlo.
      */
-    public Libro ricercaLibroPerISBN(int ISBN) throws LibroNonTrovatoException {
-        return null; // Implementazione omessa
-    };
+    public Libro ricercaLibroPerISBN(String ISBN) throws LibroNonTrovatoException {
+
+        Libro result=libri.get(ISBN);
+        if(result == null){
+            throw new LibroNonTrovatoException("Nessun libro trovato con ISBN: " + ISBN);
+        }
+        return result;
+    }
 
     /**
      * @brief Cerca libri tramite il titolo.
@@ -81,8 +110,16 @@ public class ArchivioLibri implements Serializable {
      * @post Restituisce una lista senza modificare l'archivio.
      */
     public List<Libro> ricercaLibriPerTitolo(String titolo) throws LibroNonTrovatoException {
-        return null; // Implementazione omessa
-    };
+        List <Libro> results=new ArrayList<>();
+        for(Libro l:libri.values()){
+            if(l.getTitolo().equalsIgnoreCase(titolo))
+                results.add(l);
+        }
+        if(results.isEmpty()){
+            throw new LibroNonTrovatoException("Nessun libro trovato con titolo: " + titolo);
+        }
+        return results;
+    }
 
     /**
      * @brief Cerca libri tramite l'autore.
@@ -95,15 +132,32 @@ public class ArchivioLibri implements Serializable {
      * @post Restituisce una lista senza modificare l'archivio.
      */
     public List<Libro> ricercaLibriPerAutore(String autore) throws LibroNonTrovatoException {
-        return null; // Implementazione omessa
-    };
+        List<Libro> results=new ArrayList<>();
+        for(Libro l: libri.values()){
+            if(l.getAutore().equalsIgnoreCase(autore))
+                results.add(l);
+        }
+        if(results.isEmpty()){
+            throw new LibroNonTrovatoException("Nessun libro trovato per l'autore: " + autore);
+        }
+        return results;
+    }
 
     /**
      * @brief Stampa a video o visualizza l'elenco completo dei libri.
      *
      * @post L'archivio rimane invariato.
      */
-    public void visualizzaLibri(){};
+    public void visualizzaLibri(){
+        System.out.println("ARCHIVIO LIBRI:\n");
+        if(libri.isEmpty())
+            System.out.println("L'archivio è vuoto");
+        else{
+            for(Libro l: libri.values()){
+                System.out.println(l);
+            }
+        }
+    }
 
     /**
      * @brief Salva lo stato corrente dell'archivio su file.
@@ -115,7 +169,13 @@ public class ArchivioLibri implements Serializable {
      * @pre Il percorso del file deve essere scrivibile.
      * @post I dati attuali sono persistiti su disco.
      */
-    private void salvaSuFile() throws ErroreScritturaFileException {};
+    private void salvaSuFile() throws ErroreScritturaFileException {
+        try(ObjectOutputStream out=new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)))){
+            out.writeObject(libri);
+        }catch(IOException e){
+            throw new ErroreScritturaFileException("Errore durante il salvataggio dell'archivio libri:" + e.getMessage());
+        }
+    }
 
     /**
      * @brief Carica lo stato dell'archivio da file.
@@ -127,7 +187,21 @@ public class ArchivioLibri implements Serializable {
      * @pre Il file deve esistere e contenere dati validi.
      * @post La mappa 'libri' viene popolata con i dati letti.
      */
-    private void leggiDaFile() throws ErroreLetturaFileException {};
+    private void leggiDaFile() throws ErroreLetturaFileException {
+        try(ObjectInputStream in=new ObjectInputStream(new BufferedInputStream(new FileInputStream(fileName)))){
+            Object c=in.readObject();
+            if(c instanceof Map){
+                this.libri= (Map<String, Libro>) c;
+            }
+            else{
+                throw new ErroreLetturaFileException("Il formato del file non è corretto");
+            }
+        }catch(FileNotFoundException e){
+            throw new ErroreLetturaFileException("File archivio non trovato.Inizializzazione fallita");
+        }catch(IOException | ClassNotFoundException e){
+            throw new ErroreLetturaFileException("Errore di lettura" + e.getMessage());
+        }
+    }
 
     /**
      * @brief Restituisce una rappresentazione testuale dell'intero archivio.
@@ -135,6 +209,12 @@ public class ArchivioLibri implements Serializable {
      */
     @Override
     public String toString(){
-        return "";
-    };
+        StringBuffer sb=new StringBuffer();
+        sb.append("\nARCHIVIO LIBRI");
+        for(Libro l: libri.values()){
+            sb.append("\n");
+            sb.append(l.toString());
+        }
+        return sb.toString();
+    }
 }
