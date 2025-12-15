deb3db9a36779bc3290656842cd5a4f3488bf1ff
daniel <danielvita888@gmail.com>
2025-12-15 18:57:56 +0100

    Finito

--- src/main/java/org/biblioteca_gruppo_05/Application/Launcher.java
+++ src/main/java/org/biblioteca_gruppo_05/Application/Launcher.java
@@ -2,17 +2,9 @@ package org.biblioteca_gruppo_05.Application;
 
 import org.biblioteca_gruppo_05.Eccezioni.LibroEsistenteException;
 import org.biblioteca_gruppo_05.Eccezioni.UtenteEsitenteException;
-import org.biblioteca_gruppo_05.Gestione_Libri.ArchivioLibri;
-import org.biblioteca_gruppo_05.Gestione_Libri.PopolatoreArchivio;
-import org.biblioteca_gruppo_05.Gestione_Profili.ArchivioProfili;
-import org.biblioteca_gruppo_05.Gestione_Profili.PopolatoreProfili;
 
 public class Launcher {
-    public static void main(String[] args) throws LibroEsistenteException, UtenteEsitenteException {
-        ArchivioProfili a=new ArchivioProfili("profili.bin");
-        ArchivioLibri b=new ArchivioLibri("libri.bin");
-        PopolatoreArchivio.riempiArchivio(b);
-        PopolatoreProfili.riempiArchivio(a);
+    public static void main(String[] args)  {
         HelloApplication.main(args);
     }
 }
